89a9e46804dac5ec107691167521e0e2c49caf54
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-22 20:31:48 +0530

    Delete vars/deploy.groovy

--- vars/deploy.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-    echo "installing java project"
-    sh '''
-    echo "yes"
-      sh 'cp /home/slave4/workspace/hello-world-war/target/hello-world-war-1.0.0.war /opt/apache-tomcat-9.0.85/webapps'
-    '''
-}
