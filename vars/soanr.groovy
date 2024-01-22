02c756535e0ded7d2ced034a74a9475c67103b2e
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-22 21:22:12 +0530

    Update soanr.groovy

--- vars/soanr.groovy
+++ vars/soanr.groovy
@@ -1,5 +1,5 @@
 def call() {
        // Your SonarQube scan steps go here
-       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://65.1.147.242:9000 -Dsonar.java.binaries=target/classes'
+       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar-token -Dsonar.host.url=http://65.1.147.242:9000/ -Dsonar.java.binaries=target/classes
    }
 return this
