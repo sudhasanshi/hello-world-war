a0468ffb7ad110c654dbd17c6189e3e89c485507
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-22 21:26:39 +0530

    Update soanr.groovy

--- vars/soanr.groovy
+++ vars/soanr.groovy
@@ -1,9 +1,5 @@
 def call() {
-       // Your SonarQube scan steps go here
-       
-       mvn sonar:sonar \
-  -Dsonar.projectKey=groovypipeline \
-  -Dsonar.host.url=http://65.1.147.242:9000 \
-  -Dsonar.login=09388cf9d7699b6b40c7203f8a51e23105b57be4
-   }
+       // Your SonarQube scan steps go here 
+       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar-token -Dsonar.host.url=http://65.1.147.242:9000 -Dsonar.java.binaries=target/classes'
+       }
 return this
