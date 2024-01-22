8415d6a7584f32337d0a20b3e58b82f2389a7b1b
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-22 16:10:38 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,6 +1,6 @@
 
 def call(String name= 'gitrepo'){
   sh '''
-  git clone https://github.com/tarundanda147/hello-world-war/
+  sh 'git clone https://github.com/tarundanda147/hello-world-war/'
 '''
 }
