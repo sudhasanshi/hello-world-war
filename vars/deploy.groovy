06e4dffeb77809290a407c1eb1aa849519457000
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-01-22 16:31:10 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(String name = 'tomcat'){
     echo "installing java project"
     sh '''
-      sh 'cp /home/slave4/workspace/hello-world-war/target/hello-world-war-1.0.0.war opt/apache-tomcat-9.0.85/webapps'
+      sh 'cp /home/slave4/workspace/hello-world-war/target/hello-world-war-1.0.0.war /opt/apache-tomcat-9.0.85/webapps'
     '''
 }
